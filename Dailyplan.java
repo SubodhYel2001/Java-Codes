62514fda00812228d616895a9831f51d7d6966c9
SubodhYelgandharwar <yelsubodh2001@gmail.com>
2020-07-24 00:19:36 +0530

    DailyPlans

---
+++ Dailyplan.java
@@ -0,0 +1,16 @@
+class Dailyplan {
+      int x = 10;
+	 static int y = 20;
+	 static void brush(){
+		 System.out.println("Pahila Kam Brush karne");
+		
+	 }
+	public static void main(String[] args) {
+		//Dailyplan obj = new Dailyplan();
+		Dailyplan.brush();
+		System.out.println("Nanter Bakiche");
+	}
+
+
+
+}
